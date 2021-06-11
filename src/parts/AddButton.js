122c29b3e43db50facc00ca53eef2819153998d0
burakyetistiren <burakyetistiren@hotmail.com>
2021-06-11 22:39:58 +0300

    Background has changed & disable button changed

--- src/parts/AddButton.js
+++ src/parts/AddButton.js
@@ -2,10 +2,14 @@ import PropTypes from 'prop-types'
 import './Button.css'
 
 const AddButton = ({label, add, state}) => {
-
-    return(
-        <button disabled={state} className='Button' onClick={add}>{label}</button>
-    )
+    if(state === false)
+        return(
+            <button disabled={state} className='Button' onClick={add}>{label}</button>
+        )
+    else
+        return (
+            <button disabled={state} className='DisabledButton' onClick={add}>{label}</button>
+        )
 }
 
 AddButton.defaultProps = {
