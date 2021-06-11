4552c026a9c10e674a753824b79e249f4116f922
burakyetistiren <burakyetistiren@hotmail.com>
2021-06-11 20:23:49 +0300

    UI improvements

--- src/parts/ItemInfo.js
+++ src/parts/ItemInfo.js
@@ -1,22 +1,34 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import {useHistory} from 'react-router-dom'
+import './ItemInfo.css'
 
 const ItemInfo = ({item}) => {
     const history = useHistory()
 
     return(
-        <>
-            <h2>
-                Info Page
+        <div className="Main">
+            <h2 className="pgTitle">
+                Task Detail
             </h2>
-            <h2>
-                {item.description} {item.id}
-            </h2>
-            <button onClick={() => history?.goBack()}>
+            <div>
+                <h2 className="pgText1">
+                    Task description: 
+                </h2>
+                <h2 className="pgText2">
+                    {item.description}
+                </h2>
+                <h2 className="pgText3">
+                    Task ID: 
+                </h2>
+                <h2 className="pgText4">
+                    {item.id}
+                </h2>
+            </div>
+            <button className="pgButton" onClick={() => history?.goBack()}>
                 Go back
             </button>
-        </>
+        </div>
     )
 
 }
